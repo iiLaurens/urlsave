781b5451490737dbceece215c333fd826b78dbbb
Laurens <github@laurens.xyz>
2018-05-27 12:47:49 +0200

    OS independent paths fix

--- tests/test_all.py
+++ tests/test_all.py
@@ -32,8 +32,8 @@ def driver():
 ])
 def test_eval(driver, job, page, result):
     page = os.path.join(os.getcwd(), "tests", "pages", page + ".html")
-    job =  os.path.join(os.getcwd(), "tests", "pages", job + ".txt")
-    result = os.path.join(os.getcwd(), "tests", "pages", result + ".txt")
+    job =  os.path.join(os.getcwd(), "tests", "jobs", job + ".txt")
+    result = os.path.join(os.getcwd(), "tests", "results", result + ".txt")
     
     # Load our job
     with open(job) as f:
@@ -60,8 +60,8 @@ def create_test(job, job_file, page, result_file):
         parser = Parser(job, driver = driver, keep_driver = True, test_mode = True)
         result = parser.parse()
     
-    job_file =  os.path.join(os.getcwd(), "tests", "pages", job_file + ".txt")
-    result_file = os.path.join(os.getcwd(), "tests", "pages", result_file + ".txt")
+    job_file =  os.path.join(os.getcwd(), "tests", "jobs", job_file + ".txt")
+    result_file = os.path.join(os.getcwd(), "tests", "results", result_file + ".txt")
     
     with open(job_file, 'w+') as f:
         f.write(job)
