93535c213dd64e6458b6710aff7eb569abde839f
Laurens <github@laurens.xyz>
2018-06-09 20:13:25 +0200

    Removed checking of run

--- examples/stocks/binck.py
+++ examples/stocks/binck.py
@@ -7,16 +7,10 @@ import io
 from telegram.ext import CommandHandler
 
 path = os.path.join(".", "examples", "stocks")
-processing  = os.path.join(path, "processing.txt")
 
 logging.basicConfig(filename=os.path.join(path, "errors.log"))
 
 def update(bot):
-    if os.path.exists(processing):
-        bot.send("Ik ben al bezig met een check! Heb geduld lul!")
-        return
-    
-    open(processing, 'w').close()
     try:
         with io.open(os.path.join(path, "binck.yml"),'r',encoding='utf8') as f:
             job = f.read()
@@ -40,14 +34,9 @@ def update(bot):
         
         bot.send(s, parse_mode="Markdown")
     except:
-        os.remove(processing)
         bot.send("Check niet gelukt!")
         raise
         
-    os.remove(processing)
-
-def clean():
-    os.remove(processing)
 
 def check(resp, bot):
     resp.message.reply_text("Bezig met handmatige check...")
@@ -57,7 +46,6 @@ def check(resp, bot):
 def main():
     bot = Bot(os.path.join(path, "binck.bot"))
     bot.dp.add_handler(CommandHandler("check", lambda dummy1, resp: check(resp, bot)))
-    bot.dp.add_handler(CommandHandler("clean", lambda dummy1, dummy2: clean()))
     
     schedule.every().monday.at("17:00").do(update, bot)
     schedule.every().tuesday.at("17:00").do(update, bot)
